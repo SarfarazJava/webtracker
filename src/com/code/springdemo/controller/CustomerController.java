ac0b99eea94bd23b212758b1ab89eaeaf7adb679
SarfarazAli1 <mohammedsarfarazali@ymail.com>
2016-11-13 11:56:19 +0530

    This is first commit. DAMN!

---
+++ src/com/code/springdemo/controller/CustomerController.java
@@ -0,0 +1,88 @@
+package com.code.springdemo.controller;
+
+import java.util.List;
+
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.code.springdemo.dao.CustomerDAO;
+import com.code.springdemo.entity.Customer;
+import com.code.springdemo.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	private Logger logger = Logger.getLogger(getClass());
+	
+	@GetMapping("/list")
+	public String listCustomers(Model theModel) {
+		
+		logger.info("listing customers");
+		
+		List<Customer> theCustomers = customerService.getCustomers();
+		
+		theModel.addAttribute("customers", theCustomers);
+		
+		return "list-customers";
+	}
+	
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		logger.info("Showing form for add");
+		
+		Customer theCustomer = new Customer();
+		
+		theModel.addAttribute("customer", theCustomer);
+		
+		return "customer-form";
+	}
+	
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		
+		logger.info("saving customer: " + theCustomer);
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return "redirect:/customer/list";
+	}
+	
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int theId, Model theModel) {
+		
+		logger.info("showing form for update");
+		
+		Customer theCustomer = customerService.getCustomer(theId);
+		
+		theModel.addAttribute("customer", theCustomer);
+		
+		return "customer-form";
+		
+	}
+	
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId") int theId) {
+		
+		logger.info("deleting the customer id: " + theId);
+		
+		customerService.deleteCustomer(theId);
+		
+		return "redirect:/customer/list";
+	}
+
+}
